3f9aa6ad2710eea9851346c4cc66c142bfc67589
John Todd <jtodd2854@gmail.com>
2017-08-20 12:55:47 -0500

    Static content

--- src/App.js
+++ src/App.js
@@ -2,20 +2,43 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+var Header = React.createClass({
+    render: function () {
+        return (
+            <h1 className="title">{this.props.text}</h1>
+        );
+    }
+});
+
+var SearchBar = React.createClass({
+    render: function () {
+        return (
+            <input type="search" />
+        );
+    }
+});
+
+var ProviderList = React.createClass({
+    render: function () {
+        return (
+            <ul>
+                <li>Mike Harris</li>
+                <li>Bimo Wijoyo</li>
+            </ul>
+        );
+    }
+});
+
+var App = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <Header text="Employee Directory"/>
+                <SearchBar />
+                <ProviderList />
+            </div>
+        );
+    }
+});
 
 export default App;
