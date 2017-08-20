8d3290587427883232817d6b413143a83fa92176
John Todd <jtodd2854@gmail.com>
2017-08-20 12:56:27 -0500

    Separate content from presentation, but still hardcoded

--- src/App.js
+++ src/App.js
@@ -18,12 +18,28 @@ var SearchBar = React.createClass({
     }
 });
 
+var ProviderListItem = React.createClass({
+    render: function () {
+      return (
+          <li>
+			<a href={"#providers/" + this.props.provider.id}>
+				{this.props.provider.first_name} {this.props.provider.last_name}
+			</a>
+          </li>
+      );
+    }
+});
+
 var ProviderList = React.createClass({
     render: function () {
+    	var items = this.props.providers.map(function (provider) {
+    		return (
+    			<ProviderListItem key={provider.id} provider={provider} />
+    		);
+    	});
         return (
             <ul>
-                <li>Mike Harris</li>
-                <li>Bimo Wijoyo</li>
+                {items}
             </ul>
         );
     }
@@ -31,11 +47,16 @@ var ProviderList = React.createClass({
 
 var App = React.createClass({
     render: function () {
+    	var providers = [
+    		{id:1, last_name:"Harris", first_name:"Mike"},
+    		{id:2, last_name:"Wijoyo", first_name:"Bimo"},
+    		{id:3, last_name:"Rose", first_name:"Nate"}
+    	];
         return (
             <div>
-                <Header text="Employee Directory"/>
+                <Header text="Provider Directory"/>
                 <SearchBar />
-                <ProviderList />
+                <ProviderList providers={providers} />
             </div>
         );
     }
